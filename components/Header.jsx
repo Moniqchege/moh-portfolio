c7e0c110854a6023d1643599a0908a2249464d8f
Moniqchege <quichege33@gmail.com>
2024-06-10 11:04:03 +0300

    initial commit

---
+++ components/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Link from "next/link";
+import { Button } from "./ui/button";
+
+const Header = () => {
+return (
+  <header className='py-8 xl:py-12 text-white'>
+    <div className='container mx-auto'>
+      <Link href="/">
+      <h1 className='text-4xl font-semibold'>
+        Monicah<span className='text-accent'>.</span>
+      </h1>
+      </Link>
+      <Nav />
+    </div>
+  </header>
+)
+}
+
+export default Header
